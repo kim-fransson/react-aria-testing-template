1594f2d182c58508d78329905277c58b733689fe
Kim Fransson <K39952@eon.com>
2025-05-08 10:24:51 +0200

    vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    // This means we don't have to import `describe`,
+    // `it`, and `expect` into every test file
+    globals: true,
+  },
+});
